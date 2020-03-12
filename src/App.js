8d5ceb2c71dfe1ed87e491b4a3af039c0f5db778
Katie <Katie@Sandstone.local>
2020-03-13 07:36:47 +0900

    learned Portal

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import Toggle from './ToggleRenderProps';
+import Toggle from './ToggleRPC';
+import Portal from './Portal';
+import Modal from './Modal';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
       </header>
+      <Modal>
+        <h1>Still in modal</h1>
+      </Modal>
+      <Toggle>
+        {({ on, toggle }) => (
+          <>
+            {on && <h1>Show Me</h1>}
+            <button onClick={toggle}>Show / Hide</button>
+            <Portal>{on && <h1>Hi. I'm a portal!</h1>}</Portal>
+          </>
+        )}
+      </Toggle>
     </div>
   );
 }
---
+++ src/Modal.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import Portal from './Portal';
+
+export default class Modal extends Component {
+  render() {
+    return (
+      <Portal>
+        <ModalWrapper>
+          <h1>Hi, I'm in a modal</h1>
+        </ModalWrapper>
+      </Portal>
+    );
+  }
+}
+
+const ModalWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: teal;
+`;
